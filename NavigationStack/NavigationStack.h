8852b680a3e04c0c813053981aac2799d97b8bb1
Alex.k <Alex.k@ramotion.com>
2016-06-01 12:15:31 +0300

    shared scheme

---
+++ NavigationStack/NavigationStack.h
@@ -0,0 +1,19 @@
+//
+//  NavigationStack.h
+//  NavigationStack
+//
+//  Created by Alex K. on 01/06/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavigationStack.
+FOUNDATION_EXPORT double NavigationStackVersionNumber;
+
+//! Project version string for NavigationStack.
+FOUNDATION_EXPORT const unsigned char NavigationStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationStack/PublicHeader.h>
+
+
